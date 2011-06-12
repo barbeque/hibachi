1d3fe83c24d473e7717643c5f338544169e49b45
Mike Stedman <ravuya@ravuya.com>
2011-06-11 22:31:48 -0600

    Simple animation test

---
+++ tests/spec/AnimationSpec.js
@@ -0,0 +1,23 @@
+describe("Animation", function() {
+		beforeEach(function() {
+			});
+
+		it("should loop over at the end of the animation", function() {
+			var frameMocks = [ 1, 2, 3, 4 ];
+			var animation = new Animation();
+			animation.setFrames(frameMocks);
+			animation.frameInterval = 1.0;
+			
+			expect(animation.getFrameAt(0.0)).toEqual(1);
+			expect(animation.getFrameAt(0.5)).toEqual(1);
+			expect(animation.getFrameAt(1.0)).toEqual(2);
+			expect(animation.getFrameAt(1.5)).toEqual(2);
+			expect(animation.getFrameAt(2.0)).toEqual(3);
+			expect(animation.getFrameAt(2.5)).toEqual(3);
+			expect(animation.getFrameAt(3.0)).toEqual(4);
+			expect(animation.getFrameAt(3.5)).toEqual(4);
+			expect(animation.getFrameAt(4.0)).toEqual(1);
+			expect(animation.getFrameAt(4.5)).toEqual(1);
+			expect(animation.getFrameAt(5.0)).toEqual(2);
+			});
+});
