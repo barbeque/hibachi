648910b3c100187917c920439fcc571db3249832
mike stedman <barbeque@users.noreply.github.com>
2018-02-06 16:24:28 -0700

    Auto-indent fix on keyboard spec

--- tests/spec/KeyboardSpec.js
+++ tests/spec/KeyboardSpec.js
@@ -1,39 +1,39 @@
 describe("Keyboard", function() {
-		beforeEach(function() {
-			});
-		it("must initialize", function() {
-			var keyboard = new Keyboard();
-			for(i = 0; i < 255; i++) {
-				expect(keyboard.isKeyDown(i)).toEqual(false);
-			}
-			delete keyboard;
-		});
+	beforeEach(function() {
+	});
+	it("must initialize", function() {
+		var keyboard = new Keyboard();
+		for(i = 0; i < 255; i++) {
+			expect(keyboard.isKeyDown(i)).toEqual(false);
+		}
+		delete keyboard;
+	});
 
-		it("should let me trigger the same key twice", function() {
-			var keyboard = new Keyboard();
-			var e = new Object(); // mock event message
+	it("should let me trigger the same key twice", function() {
+		var keyboard = new Keyboard();
+		var e = new Object(); // mock event message
 
-			e.keyCode = 38;
-			keyboard.onKeyDown(e);
-			expect(keyboard.isKeyDown(38)).toEqual(true);
-			expect(keyboard.isKeyDown(37)).toEqual(false);
-			keyboard.onKeyDown(e);
-			expect(keyboard.isKeyDown(38)).toEqual(true);
-			delete keyboard;
-			});
-		it("lets me press and release", function() {
-			var keyboard = new Keyboard();
-			var e = new Object();
+		e.keyCode = 38;
+		keyboard.onKeyDown(e);
+		expect(keyboard.isKeyDown(38)).toEqual(true);
+		expect(keyboard.isKeyDown(37)).toEqual(false);
+		keyboard.onKeyDown(e);
+		expect(keyboard.isKeyDown(38)).toEqual(true);
+		delete keyboard;
+	});
+	it("lets me press and release", function() {
+		var keyboard = new Keyboard();
+		var e = new Object();
 
-			e.keyCode = 38;
+		e.keyCode = 38;
 
-			expect(keyboard.isKeyDown(38)).toEqual(false);
-			keyboard.onKeyDown(e);
-			expect(keyboard.isKeyDown(38)).toEqual(true);
-			keyboard.onKeyUp(e);
-			expect(keyboard.isKeyDown(38)).toEqual(false);
-			delete keyboard;
-		});
+		expect(keyboard.isKeyDown(38)).toEqual(false);
+		keyboard.onKeyDown(e);
+		expect(keyboard.isKeyDown(38)).toEqual(true);
+		keyboard.onKeyUp(e);
+		expect(keyboard.isKeyDown(38)).toEqual(false);
+		delete keyboard;
+	});
 });
 
 function makeKeyDownObject(keyCode) {
